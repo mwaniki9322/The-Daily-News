829f783f2ae08dc30b51dc78c29c4d7abc08156a
Dennis Mwaniki <damos5776@gmail.com>
2021-08-10 19:31:00 +0300

    secret key

--- tests/test_source.py
+++ tests/test_source.py
@@ -13,7 +13,7 @@ def setUp(self):
  '''
  self.new_source = Source(1234,'wired',"Apple Walks a Privacy Tightrope to Spot Child Abuse in iCloud","With a new capability to search for illegal material not just in the cloud but on user devices, the company may have opened up a new front in the encryption wars.","https://www.wired.com/story/apple-csam-detection-icloud-photos-encryption-privacy/",'general','en','us')
 
-def test_instanc(self):
+def test_instance(self):
  self.assertTrue(isinstance(self.new_source,Source))
 
  
